229334d67fbb6ceead060af862b7605c42b74b94
anst <starininandrey@gmail.com>
2020-06-24 08:28:17 +0300

    Init project

---
+++ LogToFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Logging
+{
+    public static class LogToFile
+    {
+        public static void Log(string path, string type, string message)
+        {
+            var msg = $"{DateTime.Now} [{type}] : {message}";
+            using var file = new StreamWriter(path, true, Encoding.UTF8);
+            file.WriteLine(msg);
+        }
+    }
+}
