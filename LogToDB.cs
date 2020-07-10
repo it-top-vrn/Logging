81379caa9b083ca83b8d5c8db7b65c94774b5283
Sergey <joker3839@gmail.com>
2020-07-10 21:35:03 +0300

    LogToDB
    Добавленны методы для записи Log в БД

---
+++ LogToDB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logging
+{
+    class LogToDB
+    {
+        public string Log(string type, string idUser, string message)
+        {
+            var sql = $"INSERT INTO log VALUES ('{DateTime.Now}', '{type}', '{idUser}', '{message}')";
+            return sql;
+        }
+        public string Info(string message, string idUser)
+        {
+            return Log("INFO", idUser, message);
+        }
+        public string Error(string message, string idUser)
+        {
+            return Log("ERROR", idUser, message);
+        }
+        public string Warning(string message, string idUser)
+        {
+            return Log("WARNING", idUser, message);
+        }
+        public string Fatal(string message, string idUser)
+        {
+            return Log("FATAL", idUser, message);
+        }
+        public string Success(string message, string idUser)
+        {
+            return Log("SUCCES", idUser, message);
+        }
+    }
+}
