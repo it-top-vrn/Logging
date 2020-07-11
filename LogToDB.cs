c087b28e337534aac9ffab4a452941d1c3483662
Sergey <gexsck@outlook.com>
2020-07-11 19:10:42 +0300

    Update LogToDB.cs
    Изменил название переменной idUser на username

--- LogToDB.cs
+++ LogToDB.cs
@@ -6,30 +6,30 @@ namespace Logging
 {
     class LogToDB
     {
-        public string Log(string type, string idUser, string message)
+        public string Log(string type, string username, string message)
         {
-            var sql = $"INSERT INTO log VALUES ('{DateTime.Now}', '{type}', '{idUser}', '{message}')";
+            var sql = $"INSERT INTO log VALUES ('{DateTime.Now}', '{type}', '{username}', '{message}')";
             return sql;
         }
         public string Info(string message, string idUser)
         {
-            return Log("INFO", idUser, message);
+            return Log("INFO", username, message);
         }
         public string Error(string message, string idUser)
         {
-            return Log("ERROR", idUser, message);
+            return Log("ERROR", username, message);
         }
         public string Warning(string message, string idUser)
         {
-            return Log("WARNING", idUser, message);
+            return Log("WARNING", username, message);
         }
         public string Fatal(string message, string idUser)
         {
-            return Log("FATAL", idUser, message);
+            return Log("FATAL", username, message);
         }
         public string Success(string message, string idUser)
         {
-            return Log("SUCCES", idUser, message);
+            return Log("SUCCES", username, message);
         }
     }
 }
