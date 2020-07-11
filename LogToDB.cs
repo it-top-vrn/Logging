88bf59efffb4b88ed8c19232f64d8760fe154da9
s.olisov <s.olisov@corp.mail.ru>
2020-07-11 19:47:33 +0300

    Update LogToDB.cs
    поправки в названии аргументов методов

--- LogToDB.cs
+++ LogToDB.cs
@@ -11,23 +11,23 @@ namespace Logging
             var sql = $"INSERT INTO log VALUES ('{DateTime.Now}', '{type}', '{username}', '{message}')";
             return sql;
         }
-        public string Info(string message, string idUser)
+        public string Info(string message, string username)
         {
             return Log("INFO", username, message);
         }
-        public string Error(string message, string idUser)
+        public string Error(string message, string username)
         {
             return Log("ERROR", username, message);
         }
-        public string Warning(string message, string idUser)
+        public string Warning(string message, string username)
         {
             return Log("WARNING", username, message);
         }
-        public string Fatal(string message, string idUser)
+        public string Fatal(string message, string username)
         {
             return Log("FATAL", username, message);
         }
-        public string Success(string message, string idUser)
+        public string Success(string message, string username)
         {
             return Log("SUCCES", username, message);
         }
