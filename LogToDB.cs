4c676acfb3da6f612a6e9eccb47268c0119be886
s.olisov <s.olisov@corp.mail.ru>
2020-07-25 15:15:06 +0300

    Update LogToDB.cs
    Прошлый коммит не сохранил изменения. Исправлено.

--- LogToDB.cs
+++ LogToDB.cs
@@ -7,105 +7,87 @@ namespace Logging
 {
     public class LogToDB
     {
-        private DBconnection db = new DBconnection();
-        private string Logging(string type, string username, string message)
+        private string Logging(string type, string message, string username = "")
         {
             var sql = $"INSERT INTO log VALUES ('{DateTime.Now}', '{type}', '{username}', '{message}')";
             return sql;
         }
         public void Log(string type, string username, string message)
         {
-            if()
+            DBconnection db = new DBconnection();
+            if (db.InsertQuery(Logging(type, message, username)) != -1)
             {
-                var query = new MySqlCommand { Connection = db, CommandText = Logging(type, username, message) };
-                query.ExecuteNonQuery();
                 db.Close();
             }
             else
             {
                 var logToFile = new LogToFile();
                 logToFile.Error("Не удалось подключиться к БД");
-                logToFile.Error(Logging(type, username, message));
             }
         }
         public void Info(string message, string username)
         {
-            db.Open();
-            if (db.Ping())
+            DBconnection db = new DBconnection();
+            if (db.InsertQuery(Logging("INFO", message, username)) != -1)
             {
-                var query = new MySqlCommand { Connection = db, CommandText = Logging("INFO", username, message) };
-                query.ExecuteNonQuery();
                 db.Close();
             }
             else
             {
                 var logToFile = new LogToFile();
                 logToFile.Error("Не удалось подключиться к БД");
-                logToFile.Info(Logging("INFO", username, message));
             }
         }
         public void Error(string message, string username)
         {
-            db.Open();
-            if (db.Ping())
+            DBconnection db = new DBconnection();
+            if (db.InsertQuery(Logging("ERROR", message, username)) != -1)
             {
-                var query = new MySqlCommand { Connection = db, CommandText = Logging("ERROR", username, message) };
-                query.ExecuteNonQuery();
                 db.Close();
             }
             else
             {
                 var logToFile = new LogToFile();
                 logToFile.Error("Не удалось подключиться к БД");
-                logToFile.Error(Logging("ERROR", username, message));
             }
         }
         public void Warning(string message, string username)
         {
-            db.Open();
-            if (db.Ping())
+            DBconnection db = new DBconnection();
+            if (db.InsertQuery(Logging("WARNING", message, username)) != -1)
             {
-                var query = new MySqlCommand { Connection = db, CommandText = Logging("WARNING", username, message) };
-                query.ExecuteNonQuery();
                 db.Close();
             }
             else
             {
                 var logToFile = new LogToFile();
                 logToFile.Error("Не удалось подключиться к БД");
-                logToFile.Warning(Logging("WARNING", username, message));
             }
         }
         public void Fatal(string message, string username)
         {
-            db.Open();
-            if (db.Ping())
+            DBconnection db = new DBconnection();
+            if (db.InsertQuery(Logging("FATAL", message, username)) != -1)
             {
-                var query = new MySqlCommand { Connection = db, CommandText = Logging("FATAL", username, message) };
-                query.ExecuteNonQuery();
                 db.Close();
             }
             else
             {
                 var logToFile = new LogToFile();
-                logToFile.Error("Не удалось подключиться к БД");
                 logToFile.Fatal(Logging("FATAL", username, message));
             }
         }
         public void Success(string message, string username)
         {
-            db.Open();
-            if (db.Ping())
+            DBconnection db = new DBconnection();
+            if (db.InsertQuery(Logging("SUCCESS",message,username)) != -1)
             {
-                var query = new MySqlCommand { Connection = db, CommandText = Logging("FATAL", username, message) };
-                query.ExecuteNonQuery();
                 db.Close();
             }
             else
             {
                 var logToFile = new LogToFile();
                 logToFile.Error("Не удалось подключиться к БД");
-                logToFile.Success(Logging("SUCCESS", username, message));
             }
         }
     }
