6e40c362c10111c7084818aa75ff0ad5e25da4f9
SergeySamodurovS <god-sergey@yandex.ru>
2020-08-07 21:01:21 +0300

    Update LogToDB.cs
    Add async methods

--- LogToDB.cs
+++ LogToDB.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using DBconnect;
 
 namespace Logging
@@ -17,6 +18,7 @@ namespace Logging
         public void Log(string type, string username, string message)
         {
             DBconnection db = new DBconnection();
+            db.ConnectDB();
             if (db.InsertQuery(Logging(type, message, username)) != -1)
             {
                 db.Close();
@@ -29,6 +31,7 @@ namespace Logging
         public void Info(string message, string username)
         {
             DBconnection db = new DBconnection();
+            db.ConnectDB();
             if (db.InsertQuery(Logging("INFO", message, username)) != -1)
             {
                 db.Close();
@@ -42,6 +45,7 @@ namespace Logging
         public void Error(string message, string username)
         {
             DBconnection db = new DBconnection();
+            db.ConnectDB();
             if (db.InsertQuery(Logging("ERROR", message, username)) != -1)
             {
                 db.Close();
@@ -54,6 +58,7 @@ namespace Logging
         public void Warning(string message, string username)
         {
             DBconnection db = new DBconnection();
+            db.ConnectDB();
             if (db.InsertQuery(Logging("WARNING", message, username)) != -1)
             {
                 db.Close();
@@ -66,6 +71,7 @@ namespace Logging
         public void Fatal(string message, string username)
         {
             DBconnection db = new DBconnection();
+            db.ConnectDB();
             if (db.InsertQuery(Logging("FATAL", message, username)) != -1)
             {
                 db.Close();
@@ -78,7 +84,89 @@ namespace Logging
         public void Success(string message, string username)
         {
             DBconnection db = new DBconnection();
+            db.ConnectDB();
             if (db.InsertQuery(Logging("SUCCESS",message,username)) != -1)
+            {
+                db.Close();
+            }
+            else
+            {
+                mess?.Invoke("Не удалось подключиться к БД");
+            }
+        }
+
+        //Async methods
+        async public Task LogAsync(string type, string username, string message)
+        {
+            DBconnection db = new DBconnection();
+            db.ConnectDBAsync();
+            if (await db.InsertQueryAsync(Logging(type, message, username)) != -1)
+            {
+                db.CloseAsync();
+            }
+            else
+            {
+                mess?.Invoke("Не удалось подключиться к БД");
+            }
+        }
+        async public Task InfoAsync(string message, string username)
+        {
+            DBconnection db = new DBconnection();
+            db.ConnectDBAsync();
+            if (await db.InsertQueryAsync(Logging("INFO", message, username)) != -1)
+            {
+                db.Close();
+            }
+            else
+            {
+                var logToFile = new LogToFile();
+                logToFile.Error("Не удалось подключиться к БД");
+            }
+        }
+        async public Task ErrorAsync(string message, string username)
+        {
+            DBconnection db = new DBconnection();
+            db.ConnectDBAsync();
+            if (await db.InsertQueryAsync(Logging("ERROR", message, username)) != -1)
+            {
+                db.Close();
+            }
+            else
+            {
+                mess?.Invoke("Не удалось подключиться к БД");
+            }
+        }
+        async public Task WarningAsync(string message, string username)
+        {
+            DBconnection db = new DBconnection();
+            db.ConnectDBAsync();
+            if (await db.InsertQueryAsync(Logging("WARNING", message, username)) != -1)
+            {
+                db.Close();
+            }
+            else
+            {
+                mess?.Invoke("Не удалось подключиться к БД");
+            }
+        }
+        async public Task FatalAsync(string message, string username)
+        {
+            DBconnection db = new DBconnection();
+            db.ConnectDBAsync();
+            if (await db.InsertQueryAsync(Logging("FATAL", message, username)) != -1)
+            {
+                db.Close();
+            }
+            else
+            {
+                mess?.Invoke("Не удалось подключиться к БД");
+            }
+        }
+        async public Task SuccessAsync(string message, string username)
+        {
+            DBconnection db = new DBconnection();
+            db.ConnectDBAsync();
+            if (await db.InsertQueryAsync(Logging("SUCCESS", message, username)) != -1)
             {
                 db.Close();
             }
